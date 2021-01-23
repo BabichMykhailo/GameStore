7a8aaed13f90aa25c160348c48b216a8d57cb300
BabichMykhailo <babich.bat@gmail.com>
2021-01-23 23:57:27 +0200

    Added DAL

---
+++ GameStoreDAL/Models/Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoreDAL.Models
+{
+    public class Game
+    { 
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Discription { get; set; }
+
+        public decimal Price { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public decimal Discount { get; set; }
+    }
+}
---
+++ GameStoreDAL/Models/Publisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStoreDAL.Models
+{
+    public class Publisher
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+    }
+}
